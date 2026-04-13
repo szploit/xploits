3ece8523aad74eb882c7946f929599e12da61312
szploit <125993718+szploit@users.noreply.github.com>
2026-04-14 01:12:46 +0300

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,17 @@
+use tauri::Listener;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            let window_clone = window.clone();
+            window.listen("tauri://navigation", move |_| {
+                let _ = window_clone.set_focus();
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
