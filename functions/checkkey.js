50688c7ea221d7053422f25cd0ac48d97972b9b8
szploit <125993718+szploit@users.noreply.github.com>
2026-05-13 22:01:32 +0300

    Update checkkey.js

--- functions/checkkey.js
+++ functions/checkkey.js
@@ -1,18 +1,17 @@
 export async function onRequest(context) {
-    const url = new URL(context.request.url)
-    const key = url.searchParams.get("key")
-
-    if (!key) return new Response("invalid")
-
-    const raw = await context.env.KEYS.get(`key:${key}`)
-    if (!raw) return new Response("invalid")
-
-    const data = JSON.parse(raw)
-    if (new Date() > new Date(data.expires)) {
-        await context.env.KEYS.delete(`key:${key}`)
-        await context.env.KEYS.delete(`hwid:${data.hwid}`)
-        return new Response("expired")
-    }
-
-    return new Response("valid")
+  const url = new URL(context.request.url)
+  const key = url.searchParams.get("key")
+  if (!key) return new Response("invalid")
+  
+  const raw = await context.env.KEYS.get(`key:${key}`)
+  if (!raw) return new Response("invalid")
+  
+  const data = JSON.parse(raw)
+  if (new Date() > new Date(data.expires)) {
+    await context.env.KEYS.delete(`key:${key}`)
+    await context.env.KEYS.delete(`hwid:${data.hwid}`)
+    return new Response("expired")
+  }
+  
+  return new Response("valid")
 }
