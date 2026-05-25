c83bd32c8563d838e6fe878e1087f9e72799c582
szploit <125993718+szploit@users.noreply.github.com>
2026-05-25 10:02:23 +0300

    Create genkey3.js

---
+++ functions/genkey3.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url)
+  const auth = url.searchParams.get("auth")
+  if (auth !== context.env.ADMIN_SECRET) return new Response("forbidden", { status: 403})
+  const script = url.searchParams.get("script")
+  if (!["spawner", "duper", "instastea"].includes(script))
+    return new Response("invalid script", { status: 400 })
+  const days = parseInt(url.searchParams.get("days")) || 1
+  const newkey = crypto.randomUUID().replace(/-g/, "").toUpperCase()
+  const expiry = new Date()
+  expiry.setDate(expiry.getDate() + days)
+  await context.env.KEYS.put(newkey, JSON.stringify({expires: expiry.toISOString(), hwid: null, script}))
+  return new Response(JSON.stringify({ key: newkey, expires: expiry.toISOString(), script}), {headers: { "Content-Type": "application/json" }})
+}
