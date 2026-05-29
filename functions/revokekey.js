a1c654089e927558a9893f452695c2a346390099
szploit <125993718+szploit@users.noreply.github.com>
2026-05-29 11:30:58 +0300

    Update revokekey.js

--- functions/revokekey.js
+++ functions/revokekey.js
@@ -1,11 +1,11 @@
-export async function onRequest(context) {
-    const url = new URL(context.request.url)
+export async function onRequest(con) {
+    const url = new URL(con.request.url)
     const auth = url.searchParams.get("auth")
     const key = url.searchParams.get("key")
 
-    if (auth !== context.env.ADMIN_SECRET) return new Response("forbidden", { status: 403 })
+    if (auth !== con.env.ADMIN_SECRET) return new Response("forbidden", { status: 403 })
     if (!key) return new Response("missing key", { status: 400 })
 
-    await context.env.KEYS.delete(key)
-    return new Response("revoked")
+    await con.env.KEYS.delete(key)
+    return new Response("deleted")
 }
